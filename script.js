01df95e16c12cf673e75317dc740af17d5d8601f
melinabed <141087878+melinabed@users.noreply.github.com>
2024-05-16 13:58:15 -1000

    new javascript logic

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const Gameboard = () => {
-  const board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
-
-  let currentPlayer;
-
-  const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+//Player Factory
+const createPlayer = (name, marker) => {
+  return { name, marker };
 };
