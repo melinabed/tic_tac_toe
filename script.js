c1dce2dea83792700bc28972bfdd96f7faf0ff28
melinabed <141087878+melinabed@users.noreply.github.com>
2024-05-17 12:41:23 -1000

    added display module that shows names and markers

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //Player Factory
-
 const createPlayer = (name, marker) => {
   return { name, marker };
 };
 
 //Board Module
-
 const Gameboard = (() => {
   const gameboard = ["", "", "", "", "", "", "", "", ""];
 
@@ -19,6 +17,9 @@ const Gameboard = (() => {
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
       cell.addEventListener("click", Game.handleClick);
+      if (cell.textContent === "X") {
+        cell.style.color = "#450a0a";
+      }
     });
   };
 
@@ -34,7 +35,6 @@ const Gameboard = (() => {
 })();
 
 //Game Module
-
 const Game = (() => {
   let gameisOver;
   let currentPlayer;
@@ -43,8 +43,8 @@ const Game = (() => {
   //Starts game and declares users inputs as player1 and player2 along with markers
   const start = () => {
     players = [
-      createPlayer(document.querySelector("#player1").value, "X"),
-      createPlayer(document.querySelector("#player2").value, "O"),
+      createPlayer(document.querySelector("#player1-input").value, "X"),
+      createPlayer(document.querySelector("#player2-input").value, "O"),
     ];
 
     gameisOver = false;
@@ -55,24 +55,36 @@ const Game = (() => {
     cells.forEach((cell) => {
       cell.addEventListener("click", handleClick);
     });
+
+    displayController.displayedNames(players[0].name, players[1].name);
+    displayController.displayedMarkers(players[0].marker, players[1].marker);
   };
 
   const handleClick = (event) => {
-    if (gameisOver) {
-      return;
-    }
+    if (gameisOver) return;
 
     let index = +event.target.id;
 
+    if (Gameboard.getGameboard()[index] !== "") return;
+
     //Updates the board and html with indexes and player markers
     Gameboard.update(index, players[currentPlayer].marker);
+
+    if (checkWinner(Gameboard.getGameboard(), players[currentPlayer].marker)) {
+      gameisOver = true;
+      console.log(`${players[currentPlayer].name} won`);
+    } else if (checkDraw(Gameboard.getGameboard())) {
+      gameisOver = true;
+      console.log("Draw");
+    }
 
     //Swicthes between player1 and player2 markers
     currentPlayer = currentPlayer === 0 ? 1 : 0;
   };
 
+  //Boolean to determin if the gameboard is true or false
   const checkWinner = (board) => {
-    //Establishes possible winning combos (horizontal, vertical, and diagnol)
+    //Establishes possible winning combos (horizontal, vertical, and diagnal)
     const winCombos = [
       [0, 1, 2],
       [3, 4, 5],
@@ -83,10 +95,57 @@ const Game = (() => {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
+    for (let i = 0; i < winCombos.length; i++) {
+      const [a, b, c] = winCombos[i];
+
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return true;
+      }
+    }
+    return false;
   };
 
-  return { start, handleClick, checkWinner };
+  //If the gameboard isn't called by the checkWinner function, the game is a draw
+  //because all cells are filled
+  const checkDraw = (board) => {
+    board.every((cell) => cell !== "");
+  };
+
+  //Updates the gameboard by emptying the indices
+  const reset = () => {
+    for (let i = 0; i < 9; i++) {
+      Gameboard.update(i, "");
+    }
+    Gameboard.render();
+    gameisOver = false;
+  };
+
+  return { start, handleClick, checkWinner, checkDraw, reset };
+})();
+
+//Displays names and markers for users to see after starting game
+const displayController = (() => {
+  const displayedNames = (player1, player2) => {
+    document.querySelector("#player1-name").textContent = player1;
+    document.querySelector("#player2-name").textContent = player2;
+  };
+
+  const displayedMarkers = (player1, player2) => {
+    document.querySelector("#player1-marker").textContent = player1;
+    document.querySelector("#player2-marker").textContent = player2;
+  };
+
+  return { displayedNames, displayedMarkers };
 })();
 
 const startButton = document.querySelector("#start");
-startButton.addEventListener("click", Game.start);
+startButton.addEventListener("click", () => {
+  Game.start();
+
+  document.querySelector(".userNames").style.display = "none";
+  document.querySelector(".game-status").style.display = "flex";
+});
+
+const resetButton = document.querySelector("#reset");
+reset.addEventListener("click", Game.reset);
