257f2f883ac4c2c137d7691d42b231e933c82580
melinabed <141087878+melinabed@users.noreply.github.com>
2024-05-16 14:06:23 -1000

    initail board module

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 //Player Factory
+
 const createPlayer = (name, marker) => {
   return { name, marker };
 };
+
+//Board Module
+
+const Gameboard = (() => {
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
+
+  const render = () => {
+    let boardHTML = "";
+    gameboard.forEach((cell, index) => {
+      `<div class="${cell}" id="${index}">${cell}</div>`;
+    });
+    document.querySelector(".gameboard").innerHTML += boardHTML;
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.addEventListener("click", Game.handleClick);
+    });
+  };
+
+  return { render };
+})();
+
+//Game Module
